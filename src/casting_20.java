bba59e3b3a9064a75f01df73dc0e08134d35cf4d
Sagarmaity <sagarmaity9749@gmail.com>
2024-12-02 23:02:33 +0530

    casting

---
+++ src/casting_20.java
@@ -0,0 +1,27 @@
+public class casting_20 {
+    public static  void  main(String[]args){
+        int firsInt = Integer.MAX_VALUE;
+
+        float myfloatvalue =  865.86f;
+        int myvalue = (int) myfloatvalue;
+
+        System.out.println("before casting value is " + myfloatvalue);
+        System.out.println("after casting value is "+myvalue);
+
+        System.out.println("________________________________________________");
+        int myintvalue = 566;
+        float myfvalue = (float)myintvalue;
+        System.out.println("before casting value is " + myintvalue);
+        System.out.println("after casting value is "+myfvalue);
+        System.out.println("________________________________________________");
+
+
+        char firstchar = 'g';
+        int scnumber = (char)firstchar;
+        System.out.println("before casting value is " + firstchar);
+        System.out.println("after casting value is "+scnumber);
+
+
+    }
+
+}
