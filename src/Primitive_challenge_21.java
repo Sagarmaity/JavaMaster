02fe628bc6f89b6bdba9e6f49d5e57e10a30dfd9
Sagarmaity <sagarmaity9749@gmail.com>
2024-12-02 23:47:16 +0530

    challenge

---
+++ src/Primitive_challenge_21.java
@@ -0,0 +1,13 @@
+public class Primitive_challenge_21 {
+    public static void main(String[] args) {
+        byte firstnumber= 4;
+        short secondnumber = 12;
+        int thirdnumber = 45;
+        long fouthnumber = 50000L * 10L *(firstnumber+secondnumber+thirdnumber);
+        System.out.println("the long value is: " +fouthnumber);
+
+
+
+
+    }
+}
