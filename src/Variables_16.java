6d760fa57f32bf347a7c996df4ab7583c66259b3
Sagarmaity <sagarmaity9749@gmail.com>
2024-12-02 19:08:00 +0530

    variables

---
+++ src/Variables_16.java
@@ -0,0 +1,19 @@
+public class Variables_16 {
+    public  static  void  main(String[]args){
+        int MyFirstVariable = 4; // 4 is store in MyFirstVariable.
+        int MySecondVariable = 8; // 8 is store in MySecondVariable.
+        System.out.println("MyFirstVariable"); // in double cote every think print at it is.
+        System.out.println(MyFirstVariable); // print the value of MyFirstVariable .
+        System.out.println("MySecondVariable");
+        System.out.println(MySecondVariable);
+
+        MyFirstVariable = 40; //update ta value of MyFirstVariable 4 to 40.
+        System.out.print("after update MyFirstVariable is: ");
+        System.out.println(MyFirstVariable);
+
+        MyFirstVariable = 677; //update ta value of MyFirstVariable 40 to 677.
+        System.out.print("after update MyFirstVariable is: ");
+        System.out.println(MyFirstVariable);
+    }
+
+}
