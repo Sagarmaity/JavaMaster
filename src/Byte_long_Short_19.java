f398f612253106e96c074bdfce8eb879812eb90a
Sagarmaity <sagarmaity9749@gmail.com>
2024-12-02 20:40:24 +0530

    byte short long

---
+++ src/Byte_long_Short_19.java
@@ -0,0 +1,34 @@
+public class Byte_long_Short_19 {
+    public static  void main (String[]args){
+        byte FirstNumber = Byte.MAX_VALUE;
+        byte SecondNumber = Byte.MIN_VALUE;
+        System.out.print(" the maximum value of byte : ");
+        System.out.println(FirstNumber);
+
+        System.out.println("the minimum value of byte : " + SecondNumber);
+        System.out.println("Integer value range (" + FirstNumber +" to "+SecondNumber +")");
+
+        System.out.println("--------------------------------------------------------------------------");
+
+short firstvalue =Short.MAX_VALUE;
+short secondvalue  = Short.MIN_VALUE;
+        System.out.println(" the maximum value of byte : " + firstvalue);
+        System.out.println("the minimum value of byte : " + secondvalue);
+        System.out.println("Integer value range (" + firstvalue +" to "+secondvalue +")");
+
+        System.out.println("--------------------------------------------------------------------------");
+
+long myfnumber= Long.MAX_VALUE;
+long myfsnumber = Long.MIN_VALUE;
+long mysnumber = Long.SIZE;
+        System.out.println(" maximum value of long "+myfnumber);
+        System.out.println(" minmum value of long "+myfsnumber);
+        System.out.println("Long value range (" + myfnumber +" to "+myfsnumber +")");
+        System.out.println(" size of long "+mysnumber );
+
+
+
+
+    }
+
+}
